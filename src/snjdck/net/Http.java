fc3b443f17eb4292798ef2d01daa78634bd91eaf
snjdck <shao_kai@qq.com>
2013-06-07 23:18:59 -0700

    http更新

--- src/snjdck/net/Http.java
+++ src/snjdck/net/Http.java
@@ -1,18 +1,38 @@
 package snjdck.net;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Map;
 
 public class Http
 {
+	static private final String charsetName = "UTF-8";
+	
 	static public String Post(String path, Map<String, String> params) throws IOException
 	{
 		URL url = new URL(path);
-		URLConnection connection = url.openConnection();
-		return null;
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		
+		connection.setRequestMethod("POST");
+		connection.setDoOutput(true);
+		connection.setDoInput(true);
+		connection.setUseCaches(false);
+		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+		OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream(), charsetName);
+		osw.write("post data");
+		osw.flush();
+		osw.close();
+
+		StringBuffer buffer = new StringBuffer();
+		BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), charsetName));
+		buffer.append("\n");
+
+		return buffer.toString();
 	}
 	
 	public Http()
