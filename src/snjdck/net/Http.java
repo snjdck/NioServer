c7ae62185de5bb2e24ecdc1d232281edc0c872ec
snjdck <shao_kai@qq.com>
2013-06-18 23:27:52 -0700

    update http class

--- src/snjdck/net/Http.java
+++ src/snjdck/net/Http.java
@@ -10,6 +10,22 @@ import java.util.Map;
 
 public class Http
 {
+	public interface IHttpCallback
+	{
+		void onResult(bool ok, Object data);
+	}
+
+	public static void main(String[] args)
+	{
+		try {
+			System.out.println(Http.Get("http://www.baidu.com", null));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("==end==");
+	}
+	
 	static private final String charsetName = "UTF-8";
 	
 	static public String Post(String path, Map<String, String> params) throws IOException
@@ -35,8 +51,35 @@ public class Http
 		return buffer.toString();
 	}
 	
-	public Http()
+	static public void Get(String path, Map<String, String> params, IHttpCallback callback) throws IOException
 	{
+		new Thread(){
+			@Override
+			public void run(){
+				String result = GetImp(path, params);
+				new Runalbe(){
+					@Override
+					public void run(){
+						callback.onResult(true, result);
+					}
+				};
+			}
+		}.start();
 	}
+	
+	static private String GetImp(String path, Map<String, String> params) throws IOException
+	{
+		URL url = new URL(path);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		
+		connection.setRequestMethod("GET");
+		connection.setUseCaches(false);
+		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
+		StringBuffer buffer = new StringBuffer();
+		BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), charsetName));
+		buffer.append("hello world!~" + br.readLine());
+
+		return buffer.toString();
+	}
 }
