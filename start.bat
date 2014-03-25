96354b4b9c3ee1599fb485c63fc99aa4635e3cb3
snjdck <shao_kai@qq.com>
2014-03-25 20:34:20 +0800

    modify start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,15 @@
 @echo off
-"C:\Program Files\Java\jre7\bin\java.exe" -server -cp bin;lib/flex-messaging-core.jar;lib/flex-messaging-common.jar snjdck.server.LogicServer
+set proj_home=E:\TankGameClientCode\AAA
+set java_home=C:\Program Files\Java\jdk1.7.0_10\bin
+set main_class=Test
+set output_jar=server.zip
+
+cd /d %proj_home%
+"%java_home%\javac" -sourcepath src -d bin src/%main_class%.java
+cd bin
+"%java_home%\jar" cvfe ../%output_jar% %main_class% *.class
+cd ..
+"%java_home%\java" -jar %output_jar%
+"%java_home%\java" -cp %output_jar% %main_class%
+::"%java_home%\java" -server -cp bin;lib/flex-messaging-core.jar;lib/flex-messaging-common.jar snjdck.server.LogicServer
 @pause
