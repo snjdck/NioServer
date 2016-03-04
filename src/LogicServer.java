10f4c7e9df1c83331e1836dbc5b7af9fb41b77b8
Alex Shao <shao_kai@qq.com>
2016-03-04 18:18:46 +0800

    update

---
+++ src/LogicServer.java
@@ -0,0 +1,103 @@
+import alex.packet.PacketQueue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class LogicServer
+{
+	public static void main(String[] args) {
+		new LogicServer("127.0.0.1", 7410);
+	}
+	
+	final PacketQueue packetRecvQueue = new PacketQueue();
+	final PacketQueue packetSendQueue = new PacketQueue();
+	final Socket socket = new Socket();
+	final ByteBuffer recvBuffer = ByteBuffer.allocate(0x20000);
+	
+	public LogicServer(String host, int port)
+	{
+		try {
+			socket.connect(new InetSocketAddress(host, port));
+			init();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	void init()
+	{
+		new Thread(new ThreadRecv()).start();
+		new Thread(new ThreadSend()).start();
+		for(;;){
+			onPacketRecv(packetRecvQueue.take());
+		}
+	}
+	
+	void onPacketRecv(byte[] packet)
+	{
+	}
+	
+	class ThreadRecv implements Runnable
+	{
+		@Override
+		public void run()
+		{
+			try {
+				onRun(socket.getInputStream());
+			}catch (IOException e) {
+				e.printStackTrace();
+			} 
+		}
+		
+		void onRun(InputStream inputStream) throws IOException
+		{
+			for(;;){
+				int nBytesRead = inputStream.read(recvBuffer.array(), recvBuffer.position(), recvBuffer.remaining());
+				if(nBytesRead <= 0)
+					return;
+				recvBuffer.flip();
+				int offset = 0;
+				for(;;){
+					if(recvBuffer.remaining() < 2)
+						break;
+					int packetLen = recvBuffer.getShort(offset) & 0xFFFF;
+					if(recvBuffer.remaining() < packetLen)
+						break;
+					byte[] packet = new byte[packetLen];
+					recvBuffer.get(packet);
+					packetRecvQueue.put(packet);
+					offset += packetLen;
+				}
+				if(recvBuffer.hasRemaining()){
+					recvBuffer.compact();
+				}else{
+					recvBuffer.clear();
+				}
+			}
+		}
+	}
+
+	class ThreadSend implements Runnable
+	{
+		@Override
+		public void run()
+		{
+			try {
+				onRun(socket.getOutputStream());
+			}catch (IOException e) {
+				e.printStackTrace();
+			} 
+		}
+		
+		void onRun(OutputStream outputStream) throws IOException
+		{
+			for(;;){
+				outputStream.write(packetSendQueue.take());
+			}
+		}
+	}
+}
